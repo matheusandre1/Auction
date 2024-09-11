67b628c9e80d7f61eaad2c7dbb5c9205294cc5f5
Matheus Andre <matheusandr2@gmail.com>
2024-09-11 17:24:23 -0300

    feat: new changes

---
+++ src/Auctions/Auctions.Api/Controllers/AuctionsController.cs
@@ -0,0 +1,22 @@
+﻿using Auctions.Api.UseCases;
+using Auctions.Api.UseCases.Auctions.GetCurrent;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auctions.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCurrentAction()
+        {
+            var useCase = new GetCurrentAuctionUseCase();
+            
+            var result = useCase.Execute();
+
+            return Ok(result);
+            
+        }
+    }
+}
---
+++ src/Auctions/Auctions.Api/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Auctions.Api.Entities;
+
+namespace Auctions.Api.UseCases.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionUseCase
+    {
+        public Auction Execute()
+        {
+            return new Auction()
+            {
+                Id = 1,
+                Name = "Test",
+                Starts = DateTime.Now,
+                Ends = DateTime.Now,
+            };
+        }
+    }
+}
